43a56de4410ee5bcd5762e3ec6396ef750694821
Coldsummers <samkodi91@gmail.com>
2025-10-16 17:40:06 +0000

    edit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {
-  webpack: (config) => config,
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
